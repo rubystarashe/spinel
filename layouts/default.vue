9fcbb7a33b9a1846efe9a8a3b51cd6ac705ac7ec
RubystarAshe <rubystarashe@gmail.com>
2018-04-30 00:30:06 +0900

    first loading

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  <div id="loading" v-if="!youtube"><img src="/spinel/discord.png">loading</div>
   <nuxt v-if="youtube"/>
 </div>
 </template>
@@ -19,5 +20,11 @@ export default {
 <style>
 body {
   background: black;
+}
+#loading {
+  font-family: Angel;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
 }
 </style>
